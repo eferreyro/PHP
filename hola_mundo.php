5b0254bfe6a611c20eefceef9ed43f8e18870165
eferreyro <emafe7472@gmail.com>
2020-08-22 01:13:04 -0500

    segundo commit

--- hola_mundo.php
+++ hola_mundo.php
@@ -11,3 +11,15 @@ De multiples lineas
 echo "Hola Mundo";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?php echo "Ejemplo de HTML desde PHP" ?></h1>
+</body>
+</html>
