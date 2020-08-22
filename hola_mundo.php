42275fde25a0a6f57a71c3ad310420e67375082b
eferreyro <emafe7472@gmail.com>
2020-08-22 01:09:03 -0500

    primer commit

---
+++ hola_mundo.php
@@ -0,0 +1,13 @@
+<?php 
+//Esta es una nota simple
+
+#Este es otra nota o comentario Simple en PHP
+
+/*--------------------
+Esta es una nota
+De multiples lineas
+ --------------------*/
+
+echo "Hola Mundo";
+
+?>
